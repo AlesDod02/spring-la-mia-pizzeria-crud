13258828d43c00f2f753c605a28a0e1d820a5c7d
AlesDod02 <alessandro.dod@gmail.com>
2024-01-16 13:49:59 +0100

    starting commit

--- spring-la-mia-pizzeria-crud/src/main/resources/import.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO pizze(name, description, url, price ) VALUES ('Pizza margherita', 'Pizza sublime con pomodoro e mozzarella', 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Pizza_Margherita_stu_spivack.jpg/1280px-Pizza_Margherita_stu_spivack.jpg',9.50);
-INSERT INTO pizze(name, description, url, price ) VALUES ('Pizza diavola', 'Pizza preparata con cura con pomodoro, mozzarella e un ottimo salame calabrese', 'https://www.misya.info/wp-content/uploads/2007/08/Pizza-alla-diavola.jpg', 12.50);
